2b1b37088b51601f6a49f3e52161af563c3743cb
Chi Ben <dohuynhchiben2007@gmail.com>
2025-11-08 14:21:41 +0700

    Nop Lap1

--- Lab1/KhaiBaiBien.c
+++ Lab1/KhaiBaiBien.c
@@ -3,7 +3,7 @@
 int main(){
     int a, b;
     printf("hello, chào các bạn");
-    printf("\nnhap vao a va b: "):
+    printf("\nnhap vao a va b: ");
     scanf("%d %d", &a,&b);
      printf("Bạn vừa nhập a = %d, b = %d\n", a, b);
      return 0;
